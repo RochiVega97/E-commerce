1d26a53020b80105bfe1126c96b2570ed835f5cc
RochiVega97 <100883429+RochiVega97@users.noreply.github.com>
2023-02-26 21:51:52 -0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,6 @@ import CartWidget from './components/CartWidget'
 
 function App() {
   const [count, setCount] = useState(0)
-  const productos = [
-    {id:1 , nombre: "Notebook", descripcion:"Notebook Lenovo" , stock:4},
-    {id:2 , nombre: "Monitor", descripcion:"Monitor Led DELL" , stock:2},
-    {id:3 , nombre: "Impesora", descripcion:"Impresora  Brother" , stock:1},
-    {id:4 , nombre: "Mouse", descripcion:"Mouse DELL" , stock:4},
-    {id:5 , nombre: "Teclado", descripcion:"Teclado inalambrico DELL" , stock:10},
-  
-  ]
-  const getProductos = () =>{
-    return new Promise((resolve, reject) => {
-      if (productos.length===0){
-        reject(new Error("No hay productos que mostrar"));
-      }
-      setTimeout(() => {
-        resolve(productos);
-      }, 2000);
-    });
-  }; 
-
-  async function fetchingProductos() {
-    try {
-      const productosFetched = await getProductos();
-      console.log(productosFetched);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  fetchingProductos();
 
   const sumar = () => {
     setCount(count +1);
@@ -46,18 +17,8 @@ function App() {
       <p>{count}</p>
       <button onClick={sumar}>Sumar</button>
       <NavBar/>
-      <ItemListContainer greeting="Bienvenidos" />
+      <ItemListContainer/>
       <CartWidget/>
-      {
-        productos.map((prod) => {
-          return (
-            <div key={prod.id}>
-              <h1>{prod.nombre}</h1>
-              <h5>{prod.id}</h5>
-            </div>
-          );
-        })
-      }
     </>
     
   )
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import productos from "./data.json";
 
-const ItemListContainer = ({greeting }) => {
-    return <div>{greeting}</div>;
+const ItemListContainer = () => {
+    const getProductos = () =>{
+        return new Promise((resolve, reject) => {
+          if (productos.length===0){
+            reject(new Error("No hay productos que mostrar"));
+          }
+          setTimeout(() => {
+            resolve(productos);
+          }, 2000);
+        });
+      }; 
+    
+    async function fetchingProductos() {
+        try {
+          const productosFetched = await getProductos();
+          console.log(productosFetched);
+        } catch (err) {
+          console.log(err);
+        }
+    }
+    
+    fetchingProductos();
+
+    const cat = productos.map((productos) => {
+        return productos.categoria;
+    });
+
+    return (
+        <>
+            <ItemList productos={productos} cat={cat} />
+        </>
+    );
 };
 
 export default ItemListContainer;
