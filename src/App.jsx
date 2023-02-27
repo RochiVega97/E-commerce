cb84c7893862d28a747372a0cc7ebc3b7e422693
RochiVega97 <100883429+RochiVega97@users.noreply.github.com>
2023-02-26 21:12:17 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import CartWidget from './components/CartWidget'
 function App() {
   const [count, setCount] = useState(0)
   const productos = [
-    {id:1 , nombre: "Notebook"},
-    {id:2 , nombre: "Monitor"},
-    {id:3 , nombre: "Impesora"},
-    {id:4 , nombre: "Mouse"},
-    {id:5 , nombre: "Teclado"},
+    {id:1 , nombre: "Notebook", descripcion:"Notebook Lenovo" , stock:4},
+    {id:2 , nombre: "Monitor", descripcion:"Monitor Led DELL" , stock:2},
+    {id:3 , nombre: "Impesora", descripcion:"Impresora  Brother" , stock:1},
+    {id:4 , nombre: "Mouse", descripcion:"Mouse DELL" , stock:4},
+    {id:5 , nombre: "Teclado", descripcion:"Teclado inalambrico DELL" , stock:10},
   
   ]
   const getProductos = () =>{
